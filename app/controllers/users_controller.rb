8bcb5021fa8b513c1f835e4ca1912839a94c8f5c
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-03-19 12:40:10 -0400

    modify login routes for render

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
 
   get '/login' do
   if logged_in?
-    redirect "/stories"
+    redirect "/stories/new"
   else
   erb :"users/login"
 end
@@ -30,7 +30,7 @@ post '/login' do
   user = User.find_by(username: params[:username])
   if user && user.authenticate(params[:password])
   session[:user_id] = user.id
-  redirect "/stories"
+  redirect "/stories/new"
 else
   redirect "/login"
 end
