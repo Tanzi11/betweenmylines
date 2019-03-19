ec71c25f2f30e538c33e4e7a92472422991e9809
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-03-18 21:13:38 -0400

    create route

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,2 +1,13 @@
 class StoriesController < ApplicationController
+
+  get '/stories/new' do
+    erb :new
+  end
+
+  post '/stories' do
+    story = Story.new(params)
+    story.save
+    redirect "/stories/#{story.id}"
+  end
+
 end
