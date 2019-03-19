b03e7aff8282d529c19458e12f1bc715c28916c0
Tanzi11 <tanzima.c@fellows.thedoschool.org>
2019-03-19 16:26:22 -0400

    add routes for delete and patch

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -7,7 +7,7 @@ class StoriesController < ApplicationController
     post '/stories' do
       story = Story.new(params)
       story.save
-      redirect "/stories/#{story.story_id}"
+      redirect "/stories/#{story.id}"
     end
 
 #read
@@ -20,5 +20,23 @@ class StoriesController < ApplicationController
       @story = Story.find_by(params[:story_id])
       erb :"stories/show_story"
     end
+#update
+    get '/stories/:id/edit' do
+      @stories = Story.find_by(params[:story_id])
+      erb :"stories/edit_story"
+    end
 
+    patch '/stories/:id' do
+      @story = Article.find_by(params[:story_id])
+      @story.title = params[:title]
+      @story.description = params[:description]
+      @story.save
+      redirect to "/stories/#{@story.id}"
+    end
+#delete
+    delete '/stories/:id/delete' do
+      @story = Story.find_by(params[:story_id])
+      @story.delete
+      redirect "/stories"
+    end
 end
